da1aa5065868333d0911209cbc7bf7f0adbad759
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-06 22:09:11 -0400

    edited to not push a new instance to @@all upon initilization

--- lib/song.rb
+++ lib/song.rb
@@ -3,17 +3,22 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-    def initialize
-      @@all << self
-    end
+    # def initialize
+    #   @@all << self
+    # end
+    def save
+    self.class.all << self
+  end
+  #binding.pry
 
   def self.all
     @@all
   end
 
-  def self.create
+def self.create
     song = self.new
-   
+    song.save
+    song
   end
 
   def self.new_by_name(name)
@@ -25,6 +30,7 @@ class Song
   def self.create_by_name(name)
     song = self.new
     song.name = name
+    song.save
     song
   end
 
@@ -60,6 +66,7 @@ class Song
     new_song = self.new
     new_song.name = song_name
     new_song.artist_name = artist
+    new_song.save
     new_song
   end
   
@@ -67,9 +74,6 @@ class Song
     Song.all.clear
   end
  
-  # def save
-  #   self.class.all << self
-  # end
-  #binding.pry
+ 
 
 end
